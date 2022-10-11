4547bf0a44271344c76b08b053ef5c51491c7efb
Nurullo Sulaymonov <48081398+NurulloSulaymonov@users.noreply.github.com>
2022-10-11 17:36:06 +0500

    Initial

---
+++ Domain/Entities/Department.cs
@@ -0,0 +1,9 @@
+namespace Domain.Entities;
+
+public class Department
+{
+  //department info
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public string Description { get; set; }
+}
---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,13 @@
+namespace Domain.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Image { get; set; }
+    //department
+    public int DepartmentId { get; set; }
+    public Department Department { get; set; }
+}
